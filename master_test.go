766dcb42745b72a2aa633802808bea2580cfb871
Tatsuro Alpert <tatsuro@invisionapp.com>
2018-04-03 07:14:29 -0700

    flesh out master tests

--- master_test.go
+++ master_test.go
@@ -1,8 +1,13 @@
 package master
 
 import (
+	"errors"
+	"runtime"
 	"time"
 
+	"github.com/InVisionApp/go-logger"
+	"github.com/InVisionApp/go-logger/shims/testlog"
+	"github.com/InVisionApp/go-master/backend"
 	"github.com/InVisionApp/go-master/fakes/fakebackend"
 	"github.com/InVisionApp/go-master/safe"
 	. "github.com/onsi/ginkgo"
@@ -84,6 +89,8 @@ var _ = Describe("master", func() {
 	var (
 		m              *master
 		fakeMasterLock *fakebackend.FakeMasterLock
+		testUUID       = "test-uuid"
+		testVersion    = "test-version"
 	)
 
 	BeforeEach(func() {
@@ -93,15 +100,285 @@ var _ = Describe("master", func() {
 			heartBeatFreq: time.Millisecond,
 			lock:          fakeMasterLock,
 
-			uuid:     "test-uuid",
+			uuid:     testUUID,
+			version:  testVersion,
 			isMaster: safe.NewBool(),
+			info:     &backend.MasterInfo{},
 
 			heartBeat: director.NewFreeLooper(1, nil),
+
+			log: log.NewNoop(),
 		}
 	})
 
-	Context("start", func() {
+	Describe("start", func() {
+		It("kicks off the heartbeat", func() {
+			err := m.Start()
+			Expect(err).ToNot(HaveOccurred())
+
+			time.Sleep(time.Millisecond)
+
+			Expect(fakeMasterLock.LockCallCount()).To(Equal(1))
+		})
+
+		Context("already the master", func() {
+			BeforeEach(func() {
+				m.isMaster.SetTrue()
+			})
+
+			It("errors with already master", func() {
+				err := m.Start()
+
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("already master"))
+			})
+		})
+
+		Describe("runHeartBeat", func() {
+			Context("not the master currently", func() {
+				BeforeEach(func() {
+					m.isMaster.SetFalse()
+				})
+
+				It("tries to become the master", func() {
+					m.runHeartBeat()
+
+					Expect(fakeMasterLock.LockCallCount()).To(Equal(1))
+				})
+
+				It("sets info if becomes master", func() {
+					fakeMasterLock.LockReturns(nil)
+
+					m.runHeartBeat()
+
+					Expect(m.isMaster.Val()).To(BeTrue())
+					Expect(m.info.MasterID).To(Equal(testUUID))
+					Expect(m.info.Version).To(Equal(testVersion))
+				})
+
+				It("triggers start hook if becomes master", func() {
+					fakeMasterLock.LockReturns(nil)
+					testhook := &testHooks{}
+					m.startHook = testhook.startHook
+
+					m.runHeartBeat()
+
+					time.Sleep(time.Millisecond)
+
+					Expect(testhook.startCalls).To(Equal(1))
+				})
+
+				It("skips start hook if nil", func() {
+					m.startHook = nil
+
+					m.runHeartBeat()
+
+					Expect(m.isMaster.Val()).To(BeTrue())
+				})
+
+				It("sets nothing if fails to become master", func() {
+					fakeMasterLock.LockReturns(errors.New("failed"))
+
+					m.runHeartBeat()
+
+					Expect(m.isMaster.Val()).To(BeFalse())
+					Expect(m.info.MasterID).To(BeEmpty())
+					Expect(m.info.Version).To(BeEmpty())
+				})
+			})
+
+			Context("already the master", func() {
+				BeforeEach(func() {
+					m.isMaster.SetTrue()
+					m.info = &backend.MasterInfo{
+						MasterID: m.uuid,
+						Version:  m.version,
+					}
+				})
+
+				It("writes a heartbeat", func() {
+					m.runHeartBeat()
+
+					Expect(fakeMasterLock.WriteHeartbeatCallCount()).To(Equal(1))
+				})
+
+				Context("heartbeat fails", func() {
+					BeforeEach(func() {
+						fakeMasterLock.WriteHeartbeatReturns(errors.New("failed"))
+					})
+
+					It("sends an error down the errChan", func() {
+						ec := make(chan error)
+						m.errors = ec
+
+						m.runHeartBeat()
+
+						select {
+						case e := <-ec:
+							Expect(e.Error()).To(Equal("failed"))
+						}
+					})
+
+					It("cleans up the master", func() {
+						m.runHeartBeat()
+
+						Expect(m.isMaster.Val()).To(BeFalse())
+						Expect(m.info.MasterID).To(BeEmpty())
+						Expect(m.info.Version).To(BeEmpty())
+					})
+
+					It("calls on the stop hook", func() {
+						testhook := &testHooks{}
+						m.stopHook = testhook.stopHook
+
+						m.runHeartBeat()
+
+						time.Sleep(time.Millisecond)
+
+						Expect(testhook.stopCalls).To(Equal(1))
+					})
+
+					It("skips the stop hook if nil", func() {
+						m.stopHook = nil
+
+						m.runHeartBeat()
+
+						Expect(m.isMaster.Val()).To(BeFalse())
+					})
+				})
+			})
+		})
+
+	})
+
+	Describe("stop", func() {
+		It("does not error if not the master", func() {
+			tlog := testlog.New()
+			m.log = tlog
+
+			m.isMaster.SetFalse()
+
+			err := m.Stop()
+
+			Expect(err).ToNot(HaveOccurred())
+			Expect(tlog.Bytes()).To(ContainSubstring("not currently the master"))
+		})
+
+		Context("is the master", func() {
+			BeforeEach(func() {
+				m.isMaster.SetTrue()
+			})
+
+			It("stops without error", func() {
+				err := m.Stop()
+
+				Expect(err).ToNot(HaveOccurred())
+			})
+
+			It("resets the values", func() {
+				err := m.Stop()
+
+				Expect(err).ToNot(HaveOccurred())
+				Expect(m.isMaster.Val()).To(BeFalse())
+				Expect(m.info.MasterID).To(BeEmpty())
+				Expect(m.info.Version).To(BeEmpty())
+			})
+
+			It("does not have a goroutine leak", func() {
+				beginNum := runtime.NumGoroutine()
+				err := m.Stop()
+				endNum := runtime.NumGoroutine()
+
+				Expect(err).ToNot(HaveOccurred())
+
+				Expect(beginNum).To(Equal(endNum))
+			})
+
+			Context("backend unlock fails", func() {
+				BeforeEach(func() {
+					fakeMasterLock.UnLockReturns(errors.New("failed"))
+				})
+
+				It("does not error", func() {
+					err := m.Stop()
+
+					Expect(err).ToNot(HaveOccurred())
+				})
+
+				It("logs a message", func() {
+					tlog := testlog.New()
+					m.log = tlog
+
+					m.Stop()
+
+					Expect(tlog.Bytes()).To(ContainSubstring("failed to release lock"))
+				})
+			})
+
+			It("does not call stop hook", func() {
+				testhook := &testHooks{}
+				m.stopHook = testhook.stopHook
+
+				m.Stop()
+
+				Expect(testhook.stopCalls).To(Equal(0))
+			})
+		})
+	})
+
+	Describe("IsMaster", func() {
+		It("returns true if master", func() {
+			m.isMaster.SetTrue()
+
+			Expect(m.IsMaster()).To(BeTrue())
+		})
+
+		It("returns false if not master", func() {
+			m.isMaster.SetFalse()
+
+			Expect(m.IsMaster()).To(BeFalse())
+		})
+	})
+
+	Describe("Status", func() {
+		It("returns true if master", func() {
+			m.isMaster.SetTrue()
+
+			st, err := m.Status()
+			Expect(err).ToNot(HaveOccurred())
+
+			stat, ok := st.(map[string]interface{})
+			Expect(ok).To(BeTrue())
+			Expect(stat["is_master"]).To(BeTrue())
+		})
+
+		It("returns false if not master", func() {
+			m.isMaster.SetFalse()
+
+			st, err := m.Status()
+			Expect(err).ToNot(HaveOccurred())
+
+			stat, ok := st.(map[string]interface{})
+			Expect(ok).To(BeTrue())
+			Expect(stat["is_master"]).To(BeFalse())
+		})
+	})
+
+	Describe("scenarios", func() {
 
 	})
 
 })
+
+type testHooks struct {
+	startCalls int
+	stopCalls  int
+}
+
+func (t *testHooks) startHook() {
+	t.startCalls++
+}
+
+func (t *testHooks) stopHook() {
+	t.stopCalls++
+}
